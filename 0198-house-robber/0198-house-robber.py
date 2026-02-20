f1ee8e948ce360100405f5724c503d99ca0726ca
ShankarChalwadi <shankarchalwadi142@gmail.com>
2026-02-20 14:26:59 +0530

    Time: 4 ms (2.38%), Space: 19.4 MB (19.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,6 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp=[-1]*len(nums)
+        '''dp=[-1]*len(nums)
         return self.rec(0,nums,dp)
     def rec(self,i,nums,dp):
         if i>=len(nums):
@@ -10,4 +10,14 @@ class Solution:
         take=nums[i]+self.rec(i+2,nums,dp)
         not_take=self.rec(i+1,nums,dp)
         dp[i]=max(take,not_take)
-        return dp[i] 
+        return dp[i]''' 
+
+        if len(nums)==1:
+            return nums[0]
+        dp=[0]*len(nums)
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,len(nums)):
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+        return dp[-1]
+    
