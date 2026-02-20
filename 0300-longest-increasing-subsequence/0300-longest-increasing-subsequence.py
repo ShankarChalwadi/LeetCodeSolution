33abcb3819608c1adcd9d60106321356ee1a4e03
ShankarChalwadi <shankarchalwadi142@gmail.com>
2026-02-20 15:23:22 +0530

    Time: 1236 ms (58.86%), Space: 19.6 MB (48.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,5 +1,7 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
         dp=[1]*len(nums)
         for i in range(1,len(nums)):
             for j in range(i):
