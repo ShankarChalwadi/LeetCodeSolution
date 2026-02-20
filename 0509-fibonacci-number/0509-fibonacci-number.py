a41f69e7b145a228aef9e6256b47034eb714bd52
ShankarChalwadi <shankarchalwadi142@gmail.com>
2026-02-20 11:06:55 +0530

    Time: 41 ms (76.36%), Space: 19.3 MB (46.37%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        dp=[-1]*(n+1)
+        return self.rec(n,dp)
+    def rec(self,n,dp):
+        if n==0 or n==1:
+            return n
+        if dp[n]!=-1:
+            return dp[n]
+        dp[n]=self.rec(n-1,dp)+self.rec(n-2,dp)
+        return dp[n]
